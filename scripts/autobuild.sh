c617ed446bd10c3fd8799f8967009a981554fb5a
Ignacio Rossi <pignacio@users.noreply.github.com>
2024-02-14 15:22:45 -0300

    Add tailwind + daisyui

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -7,10 +7,11 @@ cd $(dirname "$0")/../frontend
 last_sha=""
 
 while true; do
-  new_sha=$(find . -name "*.ts" -exec sha256sum {} + | sha256sum)
+  new_sha=$(find . ../templates ../styles -not -path "./node_modules/*" \( -name "*.js" -o -name "*.html" -o -name "*.css" \) -exec sha256sum {} + | sha256sum)
   if [ "z${new_sha}" != "z${last_sha}" ]; then
     echo "$(date): SHA changed from '${last_sha}' to '${new_sha}'. Rebuilding..."
     bun build chorddb.ts --outdir ../dist --target browser
+    bun run tailwindcss -i ../styles/input.css -o ../dist/chorddb.css
     last_sha=${new_sha}
   fi
   sleep 1s;
