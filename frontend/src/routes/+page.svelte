fb1aa6767133a86ea3802a2e947c556d1e311964
Ignacio Rossi <rossi.ignacio@gmail.com>
2024-04-20 19:00:45 -0300

    Progress?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import type { PageData } from './$types';
+	import SuperDebug, { superForm, superValidate } from 'sveltekit-superforms';
+	import { valibot } from 'sveltekit-superforms/adapters';
+
+	import * as v from 'valibot';
+
+	const schema = v.object({
+		author: v.string(),
+		title: v.string(),
+		tablature: v.string()
+	});
+
+	let preForm = superValidate(valibot(schema));
+	const { form } = superForm(preForm);
 
 	export let data: PageData;
-	let author: string = '';
-	let title: string = '';
-	let tablature: string = '';
 
 	async function addSong() {
-		console.log('About to add', { author: author, title: title, tablature: tablature });
-		var data = new FormData();
-		data.append('author', author);
-		data.append('title', title);
-		data.append('contents', tablature);
-
+		console.log('About to add', form);
 		var json_data = {
-			author: author,
-			title: title,
-			contents: tablature
+			author: $form.author,
+			title: $form.title,
+			contents: $form.tablature
 		};
 
 		let response = await fetch('/api/add_song', {
@@ -41,16 +46,22 @@
 
 <form class="mt-4" on:submit|preventDefault={addSong}>
 	<div class="grid grid-cols-2 gap-4 max-w-4xl">
-		<input type="text" bind:value={author} placeholder="Author" class="input input-bordered" />
-		<input type="text" bind:value={title} placeholder="Title" class="input input-bordered" />
+		<input
+			type="text"
+			bind:value={$form.author}
+			placeholder="Author"
+			class="input input-bordered"
+		/>
+		<input type="text" bind:value={$form.title} placeholder="Title" class="input input-bordered" />
 		<textarea
 			placeholder="Tab"
-			bind:value={tablature}
+			bind:value={$form.tablature}
 			class="textarea textarea-bordered textarea-lg col-span-2 h-64"
 		></textarea>
 	</div>
 	<button class="btn btn-primary text-xl mt-4 w-32" type="submit">Add</button>
 </form>
+<SuperDebug data={$form} />
 
 <h1>Songs</h1>
 
