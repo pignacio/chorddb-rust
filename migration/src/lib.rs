f8278c6c1ea14b23f07e15130f01c618a7ae279d
Ignacio Rossi <rossi.ignacio@gmail.com>
2024-07-11 12:02:03 -0300

    Add session table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,14 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_song_and_user_tables;
+mod m20240709_020038_create_sessions_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(
-            m20220101_000001_create_song_and_user_tables::Migration,
-        )]
+        vec![
+            Box::new(m20220101_000001_create_song_and_user_tables::Migration),
+            Box::new(m20240709_020038_create_sessions_table::Migration),
+        ]
     }
 }
---
+++ migration/src/m20240709_020038_create_sessions_table.rs
@@ -0,0 +1,35 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Session::Table)
+                    .col(string(Session::Id).primary_key())
+                    .col(string(Session::UserId))
+                    .col(date_time(Session::ExpiresAt))
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Session::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Session {
+    Table,
+    Id,
+    UserId,
+    ExpiresAt,
+}
